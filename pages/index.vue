77a087d477b94b2a24c5762bf462c52e6d8289c7
Vlad Bulgakov <stick.qwe@gmail.com>
2021-12-15 13:07:01 +0300

    make header and sidebar

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,18 @@
 <template>
+<main>
   <Header/>
+  <Sidebar />
+</main>
 </template>
 
 <script>
 import Header from '@/components/Header'
+import Sidebar from '@/components/Sidebar'
 export default {
   name: 'IndexPage',
   component: {
-    Header
+    Header,
+    Sidebar
   }
 }
 </script>
