72b09e7d2d435214e59bf56f22a44150e5445a74
Vlad Bulgakov <stick.qwe@gmail.com>
2021-12-27 16:11:13 +0300

    make user page

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -6,14 +6,14 @@
     </div>
     <div class="users__list">
       <div class="users__item" v-for="(user, index) in users" :key="index">
-        <div class="users__avatar">
-          <img
-            src="https://avataaars.io/?avatarStyle=Transparent&topType=LongHairDreads&accessoriesType=Prescription02&hairColor=BrownDark&facialHairType=BeardMajestic&facialHairColor=Brown&clotheType=BlazerSweater&eyeType=Happy&eyebrowType=RaisedExcited&mouthType=Twinkle&skinColor=Tanned"
-          />
-        </div>
-        <div class="users__name">
-          {{ user.name }}
-        </div>
+        <nuxt-link :to="`users/${user.slug}`">
+          <div class="users__avatar">
+            <img :src="require(`~/assets/img/avatar/${user.avatar}`)" />
+          </div>
+          <div class="users__name">
+            {{ user.name }}
+          </div>
+        </nuxt-link>
       </div>
     </div>
   </section>
@@ -34,36 +34,46 @@ export default {
 </script>
 
 <style scoped>
+.users {
+  padding: 0;
+}
 .user__bg {
-  width: calc(100% - 221px);
+  width: calc(100% - 211px);
   height: 100vh;
   position: absolute;
   z-index: -1;
   top: 0;
-  background: linear-gradient(243.61deg, #000000 37.88%, #facf43 149.87%);
+  background: url("@/assets/img/users-bg.png");
+  background-color: #000;
+  background-position: left center;
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  padding: 0;
 }
 .user__title {
   width: 100%;
   text-align: center;
   font-size: 42px;
-  margin-top: 16px;
+  margin-top: 55px;
   color: #facf43;
 }
 .user__title h1 {
-  font-family: 'Intro';
+  font-family: "Bebas Neue";
 }
 .users__list {
   display: flex;
   flex-wrap: wrap;
   gap: 20px;
   justify-content: center;
-  margin-top: 26px;
+  margin-top: 82px;
 }
 .users__avatar img {
   width: 100%;
+  max-width: 213px;
 }
 .users__item {
-  width: 45%;
+  width: 20%;
+  text-align: center;
   transition: all 0.35s ease-in-out;
   border-color: rgba(0, 0, 0, 0.08);
   border-radius: 5px;
@@ -72,6 +82,9 @@ export default {
 }
 .users__name {
   padding: 10px;
+  color: #fff;
+  font-size: 24px;
+  margin-top: 20px;
   text-align: center;
 }
 .timer {
